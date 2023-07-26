4574a35970835e0cad3cc0b5266e606c456a0699
Isaac <98283822+O-Isaac@users.noreply.github.com>
2023-07-26 18:26:33 +0000

    feat: Add utils and docstrings

--- server.py
+++ server.py
@@ -1,12 +1,28 @@
 from translation import TranslationUtils
-from flask import Flask, send_file, request
+from flask import Flask, send_file, request, render_template_string
 from io import StringIO, BytesIO
 
 app = Flask(__name__)
-tl = TranslationUtils()
+tl = TranslationUtils(database_path="database.json")
 
 @app.route('/api/sheets/get/<scene_id>', methods=['GET'])
 def get_scene(scene_id):
+    """
+    Export translations for a scene to a Google Sheet.
+
+    Parameters:
+        scene_id (int): The ID of the scene for which translations are to be exported.
+
+    Returns:
+        str: CSV-formatted text containing translations.
+
+    Raises:
+        Exception: If an error occurs during the export process, "Not Found" is returned with a 404 status code.
+
+    Description:
+        This function exports translations for a scene specified by its `scene_id` to a Google Sheet. It generates
+        a CSV-formatted text containing translations using `tl.generate_csv_from_scene(scene_id)` and returns it.
+    """
     try:
         buffer = StringIO()
 
@@ -16,28 +32,72 @@ def get_scene(scene_id):
         csv_text = buffer.getvalue()
         buffer.seek(0)
 
-        return csv_text
+        return csv_text, 200, {'Content-Type': 'text/plain; charset=utf8'}
     except Exception as error:
+        print(error)
         return "Not Found", 404
     
 @app.route('/api/sheets/current/<scene_id>', methods=['GET'])
 def export_scene(scene_id):
+    """
+    Export scene content with HTML line breaks for translation checks.
+
+    Parameters:
+        scene_id (int): The ID of the scene to be exported.
+
+    Returns:
+        str: Scene content with HTML line breaks.
+
+    Raises:
+        Exception: If an error occurs during the export process, "Not Found" is returned with a 404 status code.
+
+    Description:
+        This function exports the content of a scene specified by its scene_id, intended for translation checks before generating the "mrg" file.
+    """
     try:
-        return tl.export_current_tl_scene(scene_id)
+        return tl.export_current_tl_scene(scene_id), 200, {'Content-Type': 'text/plain; charset=utf8'}
     except Exception as error:
+        print(error)
         return "Not Found", 404
 
-    
 @app.route('/api/database/gen', methods=['GET'])
 def renegare_db():
+    """
+    Save the current translation state in a database file.
+
+    Returns:
+        str: "Success" if the operation succeeds, "Internal Server Error" if there's an issue.
+
+    Description:
+        This function is used to save the current translation state into a database file. It calls
+        `tl.generate_db_file()` to perform the saving operation. If the operation is successful,
+        it returns "Success" with a status code 200. Otherwise, if an exception occurs during the process,
+        it returns "Internal Server Error" with a status code 503.
+    """
     try:
         tl.generate_db_file()
-        return "Success", 200
+        return "Success", 200, {'Content-Type': 'text/plain; charset=utf8'}
     except Exception as error:
         return "Internal Server Error", 503
 
 @app.route('/api/mrg/gen', methods=['GET'])
 def generate_mrg():
+    """
+    Generate an 'mrg' file with translated lines.
+
+    Returns:
+        File: The generated 'mrg' file as an attachment.
+
+    Raises:
+        Exception: If an error occurs during the generation process, "Internal Server Error" is returned with a
+                   status code 503.
+
+    Description:
+        This function generates an 'mrg' file with translated lines using `tl.generate_script_mrg()`. The generated
+        content is written to a BytesIO buffer and returned as a downloadable attachment using Flask's `send_file`
+        function. If any exceptions occur during the generation process, the function catches them and returns
+        "Internal Server Error" with a status code 503.
+    """
     try:
         buffer = BytesIO()
         file_name, file_data = tl.generate_script_mrg()
@@ -47,19 +107,35 @@ def generate_mrg():
 
         return send_file(buffer, download_name=file_name, as_attachment=True)
     except Exception as error:
+        print(error)
         return "Internal Server Error", 503
 
 @app.route('/api/sheet/pull', methods=['POST'])
 def upload_csv():
-    try:
-        csv_file = request.files['csv_file']
-        tl.process_scene_csv(csv_file)
-        
-        return "Success", 200
-    except Exception as e:
-        # En caso de que se produzca un error, devolvemos una respuesta 500
-        return "Error", 500
+    """
+    Receive and apply translations from a CSV file to the corresponding scenes.
 
+    Returns:
+        str: "Success" if the translations are successfully applied, "Error" if there's an issue.
+
+    Description:
+        This function receives a CSV file containing translated lines from a request. It iterates through the
+        uploaded files and processes each one using `tl.process_scene_csv(csv_file, fileName)`, which applies
+        the translations to the corresponding scenes. If the operation is successful, it returns "Success" with
+        a 200 status code. Otherwise, if an exception occurs during the process, it returns "Error" with a 500
+        status code.
+    """
+    try:
+        
+        for fileName in request.files:
+            csv_file = request.files[fileName]
+            tl.process_scene_csv(csv_file, fileName)
+        
+        return "Success", 200, {'Content-Type': 'text/plain; charset=utf8'}
+    except Exception as e:
+        return "Error", 500
 
 if __name__ == '__main__':
     app.run(debug=True)
+
+    
--- translation.py
+++ translation.py
@@ -1,47 +1,126 @@
-from libs.deepLuna.luna.translation_db import TranslationDb, ReadableExporter
+from libs.deepLuna.luna.translation_db import TranslationDb, ReadableExporter, RubyUtils
+from libs.deepLuna.luna.constants import Constants
+from math import isnan, ceil
+from textwrap import wrap
+from difflib import get_close_matches
+from tempfile import NamedTemporaryFile
 
 import pandas as pd
 import time
+import os
+
+class Color:
+    RED = '\033[31m'
+    GREEN = '\033[32m'
+    YELLOW = '\033[33m'
+    BLUE = '\033[34m'
+    MAGENTA = '\033[35m'
+    CYAN = '\033[36m'
+    ENDC = '\033[0m'
+
+    def __init__(self, color):
+        self.color = color
+
+    def __call__(self, text):
+        return f"{self.color}{text}{Color.ENDC}"
+
+class Utils:
+    @staticmethod
+    def check_int_or_float( value):
+        return isinstance(value, int) or isinstance(value, float)
+
+    @staticmethod
+    def is_nan(value):
+        return isnan(value)
+
+    @staticmethod
+    def adjust_text_width(text):
+        fix_text = text.replace("\r", "\n")
+        wrapped_text = wrap(fix_text, width=Constants.CHARS_PER_LINE)
+        
+        if len(wrapped_text) == 1:
+            return wrapped_text[0]
+
+        return "\n ".join(wrapped_text).strip().replace("  ", " ")
+    
+    @staticmethod
+    def adjust_quotes(text: str, text_original: str) -> str:
+        text_without_quotes = text.replace('"', '')
+        text_without_quote = text.replace('"', '')
+
+        quote = "'"
+        double_quote = '"'
+
+        if text_original.startswith(double_quote) and text_original.endswith(double_quote):
+            text = double_quote + text_without_quotes + double_quote
+        elif text_original.startswith(double_quote) and not text_original.endswith(double_quote):
+            text = double_quote + text_without_quotes
+        elif text_original.endswith(double_quote) and not text_original.startswith(double_quote):
+            text = text_without_quotes + double_quote
+
+        if text_original.startswith(quote) and text_original.endswith(quote):
+            text = quote + text_without_quote + quote
+        elif text_original.startswith(quote) and not text_original.endswith(quote):
+            text = quote + text_without_quote
+        elif text_original.endswith(quote) and not text_original.startswith(quote):
+            text = text_without_quote + quote
+
+        return text
 
 class TranslationUtils:
     "Utils for translation"
     def __init__(self, all_src_path = "allscr.mrg", script_text_path = "script_text.mrg", database_path = None):
+        RubyUtils.ENABLE_PUA_CODES = True
+
         if (database_path is not None):
             self.db_tl = TranslationDb.from_file(database_path)
         else:
             self.db_tl = TranslationDb.from_mrg(all_src_path, script_text_path)
 
+    def get_scene(self, scene_name: str):
+        scenes = self.db_tl.scene_names()
+        
+        for scene in scenes:
+            if scene_name == scene:
+                return scene
+            
+        return None
+
     def generate_csv_from_scene(self, scene_name):
         "Generate csv string to respond"
-        scenes = self.db_tl.scene_names()
-        scene_index = scenes.index(scene_name, 0)
-        scene = scenes[scene_index]
+        scene = self.get_scene(scene_name)
+        csv = [ ["hash", "english"] ]
+        text_commands = self.db_tl.lines_for_scene(scene)
 
-        csv_matrix = [ ["hash", "en_text", "es_text"] ]
-        lines = self.db_tl.lines_for_scene(scene)
+        for text_command in text_commands:
+            line_hash = text_command.jp_hash
+            line_text = self.db_tl.tl_line_for_cmd(text_command).jp_text
 
-        for line in lines:
-            line_hash = self.db_tl.tl_line_with_hash(line.jp_hash)
-            csv_matrix.append([line.jp_hash, line_hash.jp_text, ""])
+            csv.append([line_hash, line_text])
 
-        df = pd.DataFrame(csv_matrix)
+        df = pd.DataFrame(csv)
 
         return df
 
-    def process_scene_csv(self, lines_csv):
+    def process_scene_csv(self, lines_csv, sceneId):
         try:
             df = pd.read_csv(lines_csv)
 
             for _indice, fila in df.iterrows():
-                hash = fila["hash"]
-                text = fila["es_text"]
-                self.db_tl.set_translation_and_comment_for_hash(hash, text, "")
+                line_hash: str = fila["hash"]
+                line_text_translated: str = fila["spanish"]
 
-            print("[+] All rows are translated successfully ")
+
+                if Utils.check_int_or_float(line_text_translated) and Utils.is_nan(line_text_translated):
+                    print(Color(Color.YELLOW)(f"{sceneId} | {line_hash} | Missing line skipping..."))
+                    continue
+                
+                text_quotes_fix = Utils.adjust_quotes(line_text_translated, fila["english"])
+
+                self.db_tl.set_translation_and_comment_for_hash(line_hash, Utils.adjust_text_width(text_quotes_fix), "")
+                print(Color(Color.BLUE)(f"{sceneId} | {line_hash} | line successfully replaced."))
         except Exception as ex:
-            print("[!] Something wrong with translation row process")
             print(ex)
-
 
     def generate_db_file(self):
         "Regenerate database"
